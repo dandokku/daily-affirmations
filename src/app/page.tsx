6eb4f641354d6206fd49c013be8a4881577f2924
Dandokku <jesulobadaniel1@gmail.com>
2025-11-24 17:34:43 +0100

    Syntax fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <h1>Welp</h1>
-      </main>
     </div>
   );
 }
